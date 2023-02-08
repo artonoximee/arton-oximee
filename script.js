80860bcc54a610803577668f5707a2dc8730ab71
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2023-02-08 10:32:38 +0100

    added modal support

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const maisonLatapie = document.querySelector("#maison-latapie");
+// Images
+
 const neueNationalgalerie = document.querySelector("#neue-nationalgalerie");
+const maisonLatapie = document.querySelector("#maison-latapie");
+const openOrientedObjects = document.querySelector("#open-oriented-objects");
 
 const image = document.querySelector("#img-viewer");
+
+neueNationalgalerie.addEventListener("click", function() {
+  image.src = "imgs/neueNationalgalerie.jpg";
+});
 
 maisonLatapie.addEventListener("click", function() {
   image.src = "imgs/maisonLatapie.jpg";
 });
 
-neueNationalgalerie.addEventListener("click", function() {
-  image.src = "imgs/neueNationalgalerie.jpg";
+openOrientedObjects.addEventListener("click", function() {
+  image.src = "imgs/openOrientedObjects.jpg";
 });
+
+// Modal
+
+const modalOpener = document.querySelector("#modal-opener");
+const modal = document.querySelector("#info-modal");
+
+modalOpener.addEventListener('click', function (e) {
+  e.preventDefault();
+  modal.classList.add('open');
+})
+
+modal.addEventListener('click', function (e) {
+  e.preventDefault();
+  modal.classList.remove('open');
+})
