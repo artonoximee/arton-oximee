4fd3d0814d368ca05bb5fd7306932de3dd208919
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2023-03-03 14:13:17 +0100

    added new project, new scrollToTop function on mobile devices

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const images = document.querySelectorAll("img.left-pane--image")
 
 const neueNationalgalerie = document.querySelector("#neue-nationalgalerie");
 const maisonLatapie = document.querySelector("#maison-latapie");
+const immeubleVerdeaux = document.querySelector("#immeuble-verdeaux");
 const openOrientedObjects = document.querySelector("#open-oriented-objects");
 
 const imgNeueNationalgalerie = document.querySelector("#img-neueNationalgalerie");
 const imgMaisonLatapie = document.querySelector("#img-maisonLatapie");
+const imgImmeubleVerdeaux = document.querySelector("#img-immeubleVerdeaux");
 const imgOpenOrientedObjects = document.querySelector("#img-openOrientedObjects");
 
 
@@ -14,21 +16,32 @@ neueNationalgalerie.addEventListener("click", function() {
   images.forEach((image) => {
     image.classList.remove('visible');
   });
-  imgNeueNationalgalerie.classList.add('visible')
+  imgNeueNationalgalerie.classList.add('visible');
+  scrollToTop();
 });
 
 maisonLatapie.addEventListener("click", function() {
   images.forEach((image) => {
     image.classList.remove('visible');
   });
-  imgMaisonLatapie.classList.add('visible')
+  imgMaisonLatapie.classList.add('visible');
+  scrollToTop();
+});
+
+immeubleVerdeaux.addEventListener("click", function() {
+  images.forEach((image) => {
+    image.classList.remove('visible');
+  });
+  imgImmeubleVerdeaux.classList.add('visible');
+  scrollToTop();
 });
 
 openOrientedObjects.addEventListener("click", function() {
   images.forEach((image) => {
     image.classList.remove('visible');
   });
-  imgOpenOrientedObjects.classList.add('visible')
+  imgOpenOrientedObjects.classList.add('visible');
+  scrollToTop();
 });
 
 // Modal
@@ -50,3 +63,11 @@ modal.addEventListener('click', function (e) {
 
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
+
+// Scroll to top function, when mobile
+
+function scrollToTop() {
+  if (window.innerWidth < 956) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+}
